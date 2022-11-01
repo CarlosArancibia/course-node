befcde0e25ae490c68af3741500b1b53d7a158b5
CarlosArancibia <arancibiacarlos.b@gmail.com>
2022-11-01 19:42:14 -0400

    Added react

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ hbs.registerPartials(__dirname + "/public/partials");
 //send public content (static)
 app.use(express.static("public")); // This is a middleware
 
-app.get("/", (req, res) => {
-  // res.send("Home page");
-  res.render("home", {
-    title: "Course Node",
-    name: "Carlos Arancibia",
-  });
-});
+// app.get("/", (req, res) => {
+//   // res.send("Home page");
+//   res.render("home", {
+//     title: "Course Node",
+//     name: "Carlos Arancibia",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  // res.sendFile(__dirname + "/public/generic.html");
-  res.render("generic", {
-    title: "Course Node",
-    name: "Carlos Arancibia",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   // res.sendFile(__dirname + "/public/generic.html");
+//   res.render("generic", {
+//     title: "Course Node",
+//     name: "Carlos Arancibia",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  // res.sendFile(__dirname + "/public/elements.html");
-  res.render("elements", {
-    title: "Course Node",
-    name: "Carlos Arancibia",
-  });
-});
+// app.get("/elements", (req, res) => {
+//   // res.sendFile(__dirname + "/public/elements.html");
+//   res.render("elements", {
+//     title: "Course Node",
+//     name: "Carlos Arancibia",
+//   });
+// });
 
 app.get("*", (req, res) => {
   // res.send("404 | Page not found");
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => console.log(`page run in port: ${port}`));
